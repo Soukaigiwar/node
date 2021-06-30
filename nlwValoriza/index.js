8cd381eccaf8ac9d2acf7e08c4c6b4e8d267b0fd
soukaigiwar <sergiomello@gmail.com>
2021-06-30 20:13:51 -0300

    configurado typescript

---
+++ nlwValoriza/index.js
@@ -0,0 +1,4 @@
+function enviarEmail(nome, email) {
+    console.log("enviar email para " + nome + " com email " + email);
+}
+enviarEmail("Sergio", "sergiomello@gmail.com");
---
+++ nlwValoriza/index.ts
@@ -0,0 +1,5 @@
+function enviarEmail(nome: string, email: string){
+    console.log(`enviar email para ${nome} com email ${email}`);
+}
+
+enviarEmail("Sergio", "sergiomello@gmail.com");
